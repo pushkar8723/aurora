404406f29120426846e46bb3c9ed7d6e757f3249
Pushkar Anand <pushkar8723@gmail.com>
2014-04-05 02:04:55 +0530

    Update config.php

--- Web Interface/config.php
+++ Web Interface/config.php
@@ -1,6 +1,6 @@
 <?php
 // Change the following parameters according to the instructions beside them
-define("SITE_URL", "http://" . $_SERVER['HTTP_HOST'] . "/aurora");      // patth to directory
+define("SITE_URL", "http://" . $_SERVER['HTTP_HOST'] . "/aurora");      // path to directory
 define("SQL_USER", "aurora");           // Database username    
 define("SQL_PASS", "aurora");           // Database password
 define("SQL_DB", "aurora_main");     	// Database name  
