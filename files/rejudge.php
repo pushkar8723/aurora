9f558f08d659672fa8c43662cdeae0508209a36b
Pushkar Anand <pushkar8723@gmail.com>
2014-04-05 01:42:25 +0530

    major security, structure and functionality update

--- files/rejudge.php
+++
@@ -1,8 +0,0 @@
-<?php
-            $client = stream_socket_client("127.0.0.1:8724", $errno, $errorMessage);
-            if ($client === false) {
-                $_SESSION["msg"] .= "<br/>Cannot connect to Judge: Contact Admin";
-            }
-            fwrite($client, 'rejudge');
-            fclose($client);
-?>
--- judge/env/code.cpp
+++
@@ -1,8 +0,0 @@
-#include <cstdio>
-int main(){
-	int n;
-  while(scanf("%d", &n) !=EOF){
-		printf("%d\n", n*n);
-  }
-  return 0;
-}
