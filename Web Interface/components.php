797472dc5e94c790c28520ef6087961328523d27
utk1369 <utkarsh1369.11@bitmesra.ac.in>
2014-11-17 01:21:32 +0530

    Update components.php
    Accommodating changes due to introduction of static contest ranking system

--- Web Interface/components.php
+++ Web Interface/components.php
@@ -1,5 +1,6 @@
 <?php
-
+include_once 'functions.php';
+include_once 'files/Leaderboard.php';
 function loginbox() {
     if (!isset($_SESSION['loggedin'])) {
         ?>
@@ -165,51 +166,9 @@ function pagination($noofpages, $url, $page, $maxcontent) {
     }
 }
 
-function cmp($a, $b) {
-    if ($a['score'] > $b['score'])
-        return -1;
-    else if ($a['score'] < $b['score'])
-        return 1;
-    else {
-        if ($a['solved'] > $b['solved'])
-            return -1;
-        else if ($a['solved'] < $b['solved'])
-            return 1;
-        else {
-            if ($a['time'] + $a['penalty'] * 20 * 60 < $b['time'] + $b['penalty'] * 20 * 60)
-                return -1;
-            else if ($a['time'] + $a['penalty'] * 20 * 60 > $b['time'] + $b['penalty'] * 20 * 60)
-                return 1;
-            else
-                return 0;
-        }
-    }
-}
-
 function getrankings($code) {
-    $query = "select * from contest where code = '$code'";
-    $contest = DB::findOneFromQuery($query);
-    $query = "select runs.tid as tid, teamname, problems.score, submittime as time, (select count(rid) from runs r where tid = runs.tid and pid = runs.pid and result != 'AC' and result is not NULL and submittime < runs.submittime) as penalty 
-from runs, teams, problems, contest 
-where 
-teams.status = 'Normal' and runs.tid = teams.tid and problems.pid = runs.pid and
-runs.pid in (select pid from problems where pgroup ='$code') and result = 'AC' group by runs.tid, runs.pid";
-    $res = DB::findAllFromQuery($query);
-    foreach ($res as $row) {
-        if (isset($rank[$row['tid']])) {
-            $rank[$row['tid']]['time'] += ($row['time'] - $contest['starttime']);
-            $rank[$row['tid']]['score'] += $row['score'];
-            $rank[$row['tid']]['penalty'] += $row['penalty'];
-            $rank[$row['tid']]['solved'] ++;
-        } else {
-            $rank[$row['tid']]['teamname'] = $row['teamname'];
-            $rank[$row['tid']]['time'] = ($row['time'] - $contest['starttime']);
-            $rank[$row['tid']]['score'] = $row['score'];
-            $rank[$row['tid']]['penalty'] = $row['penalty'];
-            $rank[$row['tid']]['solved'] = 1;
-        }
-    }
-    usort($rank, "cmp");
-    return $rank;
+    $result =  Leaderboard::getStaticRankTableInJSON($code);
+	//echo $result;
+    return json_decode($result['ranktable'], true);	
 }
 ?>
