3746a500f3188387a703652860714116f2a5e0af
Nimesh Ghelani <nimeshghelani@gmail.com>
2014-11-19 03:51:24 +0530

    test deploy options

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 ERRORSTRING="Error. Please make sure you've indicated correct parameters"
+if [ $1 == "live" ]; then
+    DEPPATH=dcadmin@172.16.32.222:/srv/http/aurora
+elif [ $1 == "test" ]; then
+    DEPPATH=dcadmin@172.16.32.222:/srv/http/auroraTest
+else
+    echo $ERRORSTRING
+    exit 1
+fi
+
 if [ $# -eq 0 ]
 then
     echo $ERRORSTRING;
-elif [ $1 == "live" ]
-then
+else
     if [[ -z $2 ]]
     then
         echo "Running dry-run"
-        rsync --dry-run -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p9999" ./Web\ Interface/ dcadmin@172.16.32.222:/srv/http/aurora
+        rsync --dry-run -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p9999" ./Web\ Interface/ "$DEPPATH"
     elif [ $2 == "go" ]
     then
         echo "Running actual deploy"
-        rsync -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p9999" ./Web\ Interface/ dcadmin@172.16.32.222:/srv/http/aurora
+        rsync -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p9999" ./Web\ Interface/ "$DEPPATH"
     else
         echo $ERRORSTRING;
     fi
